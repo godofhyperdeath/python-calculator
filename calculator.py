cd3f4c6eb7d4669392eb64056d730bd8ff9039d7
Classic Toxin <Hydra.jf@gmail.com>
2016-05-19 18:25:43 +0930

    Added stuff and changed stuff
    Added Divide and Multiply while changing take to subtract

--- calculator.py
+++ calculator.py
@@ -1,15 +1,29 @@
 modes=[]
-modes.append("add");
+modes.append("Add");
 def add():
     n1 = int(raw_input('Enter the first number you want:'))
     n2 = int(raw_input('Enter the second number you want:'))
     return n1 + n2
-modes.append("take");
+
+modes.append("Subtract");
 def take():
     n1 = int(raw_input('Enter the first number you want:'))
     n2 = int(raw_input('Enter the second number you want:'))
     return n1 - n2
-print("calculator")
+    
+modes.append("Multiply");
+def multiply():
+    n1 = int(raw_input('Enter the first number you want:'))
+    n2 = int(raw_input('Enter the second number you want:'))
+    return n1 * n2
+    
+modes.append("Divide");
+def divide():
+    n1 = int(raw_input('Enter the first number you want:'))
+    n2 = int(raw_input('Enter the second number you want:'))
+    return n1 / n2
+    
+print("Calculator")
 print("ver 0.0.0.1")
 print("modes:")
 for x in modes: print x,
@@ -17,7 +31,12 @@ print("")
 mode = raw_input('Enter the mode you want:')
 if mode == "add":
    print(add())
-elif mode =="take":
+elif mode =="subtract":
    print(take())
+elif mode == "divide":
+    print(divide())
+elif mode == "multiply":
+    print(multiply())
 else:
-    print("unknown mode")
+    print Unknown Mode Type
+   
