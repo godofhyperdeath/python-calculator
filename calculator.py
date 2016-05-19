e3f5c90bc900c126f25ef1d145b933dcb69bba10
godofhyperdeath <pokemon25124@gmail.com>
2016-05-19 17:31:02 +0930

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,14 +1,23 @@
-
+modes=[]
+modes.append("add");
 def add():
     n1 = int(raw_input('Enter the first number you want:'))
     n2 = int(raw_input('Enter the second number you want:'))
     return n1 + n2
-
+modes.append("take");
+def take():
+    n1 = int(raw_input('Enter the first number you want:'))
+    n2 = int(raw_input('Enter the second number you want:'))
+    return n1 - n2
 print("calculator")
-print("ver 0.0.0")
-print("modes: add")
+print("ver 0.0.0.1")
+print("modes:")
+for x in modes: print x,
+print("")
 mode = raw_input('Enter the mode you want:')
 if mode == "add":
    print(add())
+elif mode =="take":
+   print(take())
 else:
     print("unknown mode")
