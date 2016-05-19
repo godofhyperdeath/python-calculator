dd88fbff6a11df363612e1df2b3bad68cf9e9ba7
Classic Toxin <Hydra.jf@gmail.com>
2016-05-19 18:28:13 +0930

    Fixed a capilsation

--- calculator.py
+++ calculator.py
@@ -1,31 +1,27 @@
 modes=[]
 modes.append("Add");
+modes.append("Subtract");
+modes.append("Multiply");
+modes.append("Divide");
 def add():
     n1 = int(raw_input('Enter the first number you want:'))
     n2 = int(raw_input('Enter the second number you want:'))
     return n1 + n2
-
-modes.append("Subtract");
 def take():
     n1 = int(raw_input('Enter the first number you want:'))
     n2 = int(raw_input('Enter the second number you want:'))
     return n1 - n2
-    
-modes.append("Multiply");
 def multiply():
     n1 = int(raw_input('Enter the first number you want:'))
     n2 = int(raw_input('Enter the second number you want:'))
     return n1 * n2
-    
-modes.append("Divide");
 def divide():
     n1 = int(raw_input('Enter the first number you want:'))
     n2 = int(raw_input('Enter the second number you want:'))
     return n1 / n2
-    
-print("Calculator")
-print("ver 0.0.0.1")
-print("modes:")
+version = "0.0.0.1"
+print("Ver " + version)
+print("Modes:")
 for x in modes: print x,
 print("")
 mode = raw_input('Enter the mode you want:')
