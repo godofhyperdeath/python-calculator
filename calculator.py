d3c253e7adbf53371981567e7b9fd3130552b4db
godofhyperdeath <pokemon25124@gmail.com>
2016-05-19 16:41:16 +0930

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,16 @@
+
+def add():
+    n1 = int(raw_input('Enter the first number you want:'))
+    n2 = int(raw_input('Enter the second number you want:'))
+    return n1 + n2
+
 print("calculator")
-print("ver 0.0.0")
+print("ver 0.0.0.0")
 print("modes: add")
 mode = raw_input('Enter the mode you want:')
+if mode == "add":
+   res = add()
+   print(res)
+else:
+    print("1")
+
